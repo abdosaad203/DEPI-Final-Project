3e4977cd4cc0ffc73c687d487d4f9904b0f96b51
abdosaad203 <abdosaad4900@gmail.com>
2026-05-19 23:44:23 +0300

    fix httpss

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -1,5 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080, listenOptions =>
+    {
+        listenOptions.Protocols =
+            Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
+});
 builder.AddBasicServiceDefaults();
 builder.AddApplicationServices();
 
